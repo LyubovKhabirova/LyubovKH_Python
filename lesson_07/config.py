29c4323077335e1d2332953e9cbb97efe022ec99
lubovhab <lubovhabirova@gmail.com>
2026-07-20 00:44:32 +0500

    Добавлены автотесты для калькулятра и интернет-магазина, используя паттерн Page Object

---
+++ lesson_07/config.py
@@ -0,0 +1,29 @@
+# Базовый URL сайта Калькулятор
+CALC_URL = \
+    "https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html"
+
+# URL страницы авторизации
+LOGIN_URL = "https://www.saucedemo.com/"
+
+# Логин и пароль пользователя
+USERNAME_LOGIN = "standard_user"
+PASSWORD_LOGIN = "secret_sauce"
+
+# URL главной страницы магазина
+MAIN_URL = "https://www.saucedemo.com/inventory.html"
+
+# Список товаров для покупки
+PRODUCTS = ["Sauce Labs Backpack",
+            "Sauce Labs Bolt T-Shirt",
+            "Sauce Labs Onesie"
+            ]
+
+# URL страницы корзины
+CART_URL = "https://www.saucedemo.com/cart.html"
+
+# URL страницы оформления покупки
+CHECKOUT_URL = "https://www.saucedemo.com/checkout-step-one.html"
+
+# Сколько секунд ждать элементы
+TIMEOUT = 10
+TIMEOUT_LONG = 45
